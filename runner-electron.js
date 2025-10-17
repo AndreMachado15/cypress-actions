0b107f46f7d622443a0affa2856e12960c22abb2
AndreMachado15 <andrehenrique151504@gmail.com>
2025-10-17 12:19:09 -0300

    Create runner-electron.js

---
+++ runner-electron.js
@@ -0,0 +1,17 @@
+const cypress = require('cypress')
+const tesults = require('cypress-tesults-reporter');
+
+const TOKEN_ELECTRON = process.env.TESULTS_TOKEN_ELECTRON;
+
+cypress.run({
+  // specs to run here
+})
+.then((results) => {
+  const args = {
+    target: TOKEN_ELECTRON,
+  }
+  tesults.results(results, args);
+})
+.catch((err) => {
+ console.error(err)
+})
